7d92a7ff251cf1e1b0a95a152015f277f387258e
Khanhlinh0209 <khanhlinhwork1110@gmail.com>
2026-02-10 10:40:22 +0700

    refactor: simplify App component and integrate MainLayout
    - Removed unnecessary imports and code from App component.
    - Integrated MainLayout for improved structure.
    - Updated main.tsx to use RouterProvider for routing management.

---
+++ Frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import MainLayout from "@/layouts/MainLayout";
+
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/map" replace />,
+      },
+      
+    ],
+  },
+]);
