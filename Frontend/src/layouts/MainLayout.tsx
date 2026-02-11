19d291eebbf9629acccc2d49994a2dfd85f63f1b
Khanhlinh0209 <khanhlinhwork1110@gmail.com>
2026-02-11 12:51:08 +0700

    refactor: update layout and styling structure
    - Removed unused imports and cleaned up the App component.
    - Commented out the body styles and button hover styles in index.css for future adjustments.
    - Adjusted MainLayout to remove the container class and added padding for better layout management.
    - Integrated HomePage as the default child route in AppRoutes for improved navigation.

--- Frontend/src/layouts/MainLayout.tsx
+++ Frontend/src/layouts/MainLayout.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
-function MainLayout() {
-    return (
-        <>
-            <Header />
-            <main className="flex-grow container mx-auto px-4 py-6">
-                <Outlet />
-            </main>
-        </>
 
-    );
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      {/* KHÔNG container ở đây */}
+      <main className="flex-grow pt-16">
+        <Outlet />
+      </main>
+    </>
+  );
 }
 
 export default MainLayout;
--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,14 +1,17 @@
-import MainLayout from "@/layouts/MainLayout";
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "@/layouts/MainLayout";
+import HomePage from "@/pages/HomePage";
+
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     children: [
-
-
-
+      {
+        index: true, // trang /
+        element: <HomePage />,
+      },
     ],
   },
 ]);
